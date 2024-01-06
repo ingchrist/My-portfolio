8d2c78cc4a80d879b02b5a9a57335205a0c5e5f7
ingchrist <cdjinguet@gmail.com>
2024-01-06 18:09:02 +0100

    files added

--- README.md
+++
@@ -1,2 +0,0 @@
-# My-portfolio
-My personal projects and internships with certificates 
--- src/utils.js
+++
@@ -1,3 +0,0 @@
-export const getImageurl = (path) => {
-    return new URL('/assets'/$(path) , import.meta.url).href;
-};  
