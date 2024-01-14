20ec51eff7d32ba8ad7ddb0aa1af44508be2efd4
ingchrist <cdjinguet@gmail.com>
2024-01-14 14:07:24 +0100

    files added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm ingchrist</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with some experience using React and
+          I'm a full-stack developer with experience using React and
           NodeJS. Reach out if you'd like to learn more! 
         </p>
         <a href="mailto:myemail@email.com" className={styles.contactBtn}>
